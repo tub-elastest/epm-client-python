9db7c4d13167e2e3f594b89430cbe79a2a655a10
radoslavvlaskovski <radoslav.vlaskovsky@gmail.com>
2018-02-27 18:30:46 +0100

    Pre-tag commit: 0.8.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "epm_client"
-VERSION = "1.0.0"
+VERSION = "0.8.0"
 # To install the library, run the following
 #
 # python setup.py install
